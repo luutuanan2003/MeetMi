6486d7eb676cfa3a9a7ec8bb5dc763d5fac27cb1
anluu <luutuanan2003@gmail.com>
2024-01-05 00:49:00 +0700

    Login UI has been created by An - 5/1/2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.meetmi"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.meetmi"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
