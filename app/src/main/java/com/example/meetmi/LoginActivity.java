b6114e40597ccc486d3af880b7ae474077b96413
ElwizScott <s3878260@rmit.edu.vn>
2024-01-05 12:56:25 +0700

    Upper background changed

--- app/src/main/java/com/example/meetmi/LoginActivity.java
+++ app/src/main/java/com/example/meetmi/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.meetmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWindow().setStatusBarColor(Color.parseColor("#1F1F1F"));
     }
 }
