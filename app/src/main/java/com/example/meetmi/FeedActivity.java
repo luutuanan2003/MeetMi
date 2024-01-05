073f48b8f1366a66da838538a68a2ff14f85f29d
anluu <luutuanan2003@gmail.com>
2024-01-05 12:07:17 +0700

    NewsFeed UI has been added by An 5/1/2024

---
+++ app/src/main/java/com/example/meetmi/FeedActivity.java
@@ -0,0 +1,13 @@
+package com.example.meetmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
